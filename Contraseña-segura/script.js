d3f22f21fdcf2f2cebf388665734284bc899ec9d
Facundo Vera <facundovera.dev@gmail.com>
2025-09-25 16:41:32 -0300

    Cree la carpeta Contraseña-segura para hacer el primer ejercicio

---
+++ Contraseña-segura/script.js
@@ -0,0 +1,15 @@
+//&Ejercicio integrador: Contraseña segura
+
+// *Crea un programa en javascript que pida al usuario que ingrese una contraseña.
+// ?El programa debe:
+// - Revisar que la contraseña tenga al menos 6 caracteres
+// - Usar un bucle para recorrer cada caracter y verificar:
+//  - Si contiene al menos una mayúscula
+//  - Si contiene al menos un número.
+// - Si cumple con todos los requisitos mostrar 'Contraseña válida ✅'
+// - Si no cumple, mostrrar "Contraseña inválida ❌" y explicar porqué.
+
+//!ALGORITMO 
+//Datos:pedir contraseña al usuario 
+//Proceso: revisar que la contraseña cumpla con los requisitos
+//Final:si cumple con todos los requisitos mostrar un mensaje y si no cumple tambien 
