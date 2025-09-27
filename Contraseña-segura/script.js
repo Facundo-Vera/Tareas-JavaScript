04b7f5a8fa20e3959b4ee91faa2bd11acb1ea373
Facundo Vera <facundovera.dev@gmail.com>
2025-09-27 13:44:51 -0300

    Realice el el ejercicio de Contraseña Valida

--- Contraseña-segura/script.js
+++ Contraseña-segura/script.js
@@ -13,3 +13,33 @@
 //Datos:pedir contraseña al usuario 
 //Proceso: revisar que la contraseña cumpla con los requisitos
 //Final:si cumple con todos los requisitos mostrar un mensaje y si no cumple tambien 
+
+
+
+let contraseña = prompt("Ingrese una contraseña ")
+let tieneMayuscula= false;
+let tieneNumero = false;
+if (contraseña.length < 6){
+    console.log("Ingrese una contraseña de almenos 6 caracteres")
+}
+
+for (let i = 0 ; i <contraseña.length; i++){
+    caracter = contraseña[i]
+    if(caracter === caracter.toUpperCase() && caracter !== caracter.toLowerCase()){
+       tieneMayuscula = true;
+       
+    }
+    if(!isNaN (caracter)){
+      tieneNumero = true;
+    }
+  
+}
+if(contraseña.length > 6 && tieneMayuscula && tieneNumero){
+    console.log("Contraseña valida")
+
+}else if (!tieneMayuscula){
+    console.log("Contraseña inválida ❌ debe contener al meno una Mayuscula")
+}
+else if (!tieneNumero){
+    console.log("Contraseña inválida ❌ debe contener al meno un Numero ")
+}
