1fc6289666694c53d50ef159add4c017dc10a789
Facundo Vera <facundovera.dev@gmail.com>
2025-10-02 16:30:44 -0300

    Ejercicio 1 del tp-3

---
+++ TP-3/arrays.js
@@ -0,0 +1,23 @@
+//& 1- Crear un array llamado meses y que almacene el nombre de los doce meses del año.
+// ?Mostrar por pantalla en forma de lista los doce nombres del arreglo.
+
+// let meses = [
+//   "Enero",
+//   "Febrero",
+//   "Marzo",
+//   "Abril",
+//   "Mayo",
+//   "Junio",
+//   "Julio",
+//   "Agosto",
+//   "Septiembre",
+//   "Octubre",
+//   "Noviembre",
+//   "Diciembre",
+// ];
+
+// for(let i = 0 ;i <meses.length;i++){
+//     console.log(`${i + 1} - ${meses[i]}`)
+// }
+
+
