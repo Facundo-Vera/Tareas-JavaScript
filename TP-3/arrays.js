50428b462ea91f352b8ae2799bf43ad2c9ba9c11
Facundo Vera <facundovera.dev@gmail.com>
2025-10-04 15:17:22 -0300

    TP-3

--- TP-3/arrays.js
+++ TP-3/arrays.js
@@ -29,15 +29,15 @@
 //* 2 dados
 // *numeros aleatorios
 //  sumar el resultado y almacenarlos en un //*array (vacio) el numero de apariciones de la suma,
-// repetir 60 veces la operacion
+//* repetir 60 veces la operacion
 
-let arrayDeSumas = [];
+let apariciones = [];
 let contador = 0;
 while (contador < 60) {
   let dado1 = Math.floor(Math.random() * 6) + 1;
   let dado2 = Math.floor(Math.random() * 6) + 1;
   let suma = dado1 + dado2;
-  arrayDeSumas.push(suma)
+  apariciones.push(suma)
   contador ++;
 }
-console.log(arrayDeSumas)
+console.log(apariciones)
