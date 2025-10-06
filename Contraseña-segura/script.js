81d9879ee693539859d256b3c9b6115b259b723e
Facundo Vera <facundovera.dev@gmail.com>
2025-10-06 16:39:56 -0300

    Echas las correcciones del ejercicio Contraseña-segura

--- Contraseña-segura/script.js
+++ Contraseña-segura/script.js
@@ -9,37 +9,37 @@
 // - Si cumple con todos los requisitos mostrar 'Contraseña válida ✅'
 // - Si no cumple, mostrrar "Contraseña inválida ❌" y explicar porqué.
 
-//!ALGORITMO 
-//Datos:pedir contraseña al usuario 
+//!ALGORITMO
+//Datos:pedir contraseña al usuario
 //Proceso: revisar que la contraseña cumpla con los requisitos
-//Final:si cumple con todos los requisitos mostrar un mensaje y si no cumple tambien 
+//Final:si cumple con todos los requisitos mostrar un mensaje y si no cumple tambien
 
-
-
-let contraseña = prompt("Ingrese una contraseña ")
-let tieneMayuscula= false;
+let contraseña = prompt("Ingrese una contraseña ");
+let tieneMayuscula = false;
 let tieneNumero = false;
-if (contraseña.length < 6){
-    console.log("Ingrese una contraseña de almenos 6 caracteres")
-}
-
-for (let i = 0 ; i <contraseña.length; i++){
-    caracter = contraseña[i]
-    if(caracter === caracter.toUpperCase() && caracter !== caracter.toLowerCase()){
-       tieneMayuscula = true;
-       
+if (contraseña.length < 6) {
+  console.log("Ingrese una contraseña de almenos 6 caracteres");
+} else {
+  for (let i = 0; i < contraseña.length; i++) {
+    caracter = contraseña[i];
+    if (
+      caracter === caracter.toUpperCase() &&
+      caracter !== caracter.toLowerCase()
+    ) {
+      tieneMayuscula = true;
     }
-    if(!isNaN (caracter)){
+    if (!isNaN(caracter)) {
       tieneNumero = true;
     }
-  
+  }
 }
-if(contraseña.length > 6 && tieneMayuscula && tieneNumero){
-    console.log("Contraseña valida")
 
-}else if (!tieneMayuscula){
-    console.log("Contraseña inválida ❌ debe contener al meno una Mayuscula")
+if (contraseña.length >= 6 && tieneMayuscula && tieneNumero) {
+  console.log("Contraseña valida");
+} else if (!tieneMayuscula) {
+  console.log("Contraseña inválida ❌ debe contener al menos una Mayuscula");
+} else if (!tieneNumero) {
+  console.log("Contraseña inválida ❌ debe contener al menos un Numero ");
 }
-else if (!tieneNumero){
-    console.log("Contraseña inválida ❌ debe contener al meno un Numero ")
-}
+
+
